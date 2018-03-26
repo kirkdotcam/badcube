d2a472d8b8db7a59d9e5def19bf78b30688e7508
camkirk <camden.kirkland@gmail.com>
2018-03-25 21:53:55 -0500

    starting schemas

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ fs.readdir(collecDirectory, (err,files) => {
 		if (nameArray[nameArray.length-1]==='json'){
 			let tmp = require(path.join(__dirname,collecDirectory,nameArray[0]))
 			if(typeof tmp === "array"||typeof tmp ==="object"){
-
 					collection[nameArray[0]]= tmp
 					collecNames.push(filename)
 				}
@@ -26,3 +25,29 @@ fs.readdir(collecDirectory, (err,files) => {
 	exports.collections = collection
 });
 
+function Schema(name,collectionRef){
+	if(!this instanceof Schema){
+		return new Schema(name)
+	}
+
+	let tempName = name.charAt(0).toUpperCase() + string.slice(1)
+	
+	this.name = tempName
+	this.collection = collectionRef
+	this.find = function(queryObj){}
+	this.findAll = function(queryObj){}
+	this.insert = function(newObj){
+		//grab collection
+		//add new object
+		//rewrite file
+		
+	}
+	this.insertMany = function(array){}
+	this.update = function(queryObj, newObj){}
+	this.delete = function(queryObj){}
+
+	//i/o for future implementation
+	//this.toMongo = function(){}
+	//this.toSQL = function(){}
+
+}
