802a6b16b1fd47de56b24b70fe0182c7588f64b9
CamKirk <camdenkirkland@hotmail.com>
2018-04-06 17:24:07 -0500

    Renaming this.name in Model to this.modelName

--- index.js
+++ index.js
@@ -3,76 +3,76 @@ let path = require('path');
 let collecDirectory = './collections';
 let collection = {};
 
-if(!fs.existsSync(collecDirectory)){
+if (!fs.existsSync(collecDirectory)) {
 	fs.mkdirSync(collecDirectory);
 }
 
 
-function Model(name,collectionRef,collectionObj){
+function Model(modelName, collectionRef, collectionObj) {
 	// if(!this instanceof this.Model){
-	// 	return new Model(name)
+	// 	return new Model(modelName)
 	// }
 
 
-	this.name = name;
+	this.modelName = modelName;
 	this.collectionRef = collectionRef;
 	this.collection = collectionObj;
 
-	this.find = function(queryObj){
-		return this.collection.find((element,index)=>{
+	this.find = function (queryObj) {
+		return this.collection.find((element, index) => {
 			let searchKey = Object.keys(queryObj)[0];
 			let searchValue = Object.values(queryObj)[0];
-			return  element[searchKey] === searchValue;
+			return element[searchKey] === searchValue;
 		});
 	};
 
-	this.findAll = function(queryObj){
-		return this.collection.filter((element)=>{
+	this.findAll = function (queryObj) {
+		return this.collection.filter((element) => {
 			let searchKey = Object.keys(queryObj)[0];
 			let searchValue = Object.values(queryObj)[0];
-			return  element[searchKey] === searchValue;
+			return element[searchKey] === searchValue;
 		});
 	};
 
-	this.rewrite = function(){
-		fs.writeFileSync(this.collectionRef,JSON.stringify(this.collection,null,2));
-		this.collection=this.findAll({});
+	this.rewrite = function () {
+		fs.writeFileSync(this.collectionRef, JSON.stringify(this.collection, null, 2));
+		this.collection = this.findAll({});
 	};
 
-	this.insert = function(newObj){
-		if(typeof newObj === 'object'){
+	this.insert = function (newObj) {
+		if (typeof newObj === 'object') {
 			this.collection.push(newObj);
 			this.rewrite();
 			return newObj;
 		}
-		else{throw "not an object"};
+		else { throw "not an object" };
 	};
 
-	this.insertMany = function(arr){
-		if(Array.isArray(arr)){
-			arr.forEach((item)=>{
-				if(typeof item === 'object' && !Array.isArray(item)){
+	this.insertMany = function (arr) {
+		if (Array.isArray(arr)) {
+			arr.forEach((item) => {
+				if (typeof item === 'object' && !Array.isArray(item)) {
 					this.collection.push(item);
 				}
 			});
 			this.rewrite();
 			return arr;
 		}
-		else{throw "Did not insertMany an Array"};
+		else { throw "Did not insertMany an Array" };
 	};
 
-	this.update = function(queryObj, newObj){
+	this.update = function (queryObj, newObj) {
 		let queryResult = this.find(queryObj);
 		let queryLoc = this.collection.indexOf(queryResult);
-		let updatedObj = Object.assign(queryResult,newObj);
-		this.collection.splice(queryLoc,1,updatedObj);
+		let updatedObj = Object.assign(queryResult, newObj);
+		this.collection.splice(queryLoc, 1, updatedObj);
 		this.rewrite();
 		return queryObj;
 	};
 
-	this.delete = function(queryObj){
+	this.delete = function (queryObj) {
 		let deleteLoc = this.collection.indexOf(this.find(queryObj));
-		this.collection.splice(deleteLoc,1);
+		this.collection.splice(deleteLoc, 1);
 		this.rewrite()
 		return queryObj;
 	};
@@ -85,26 +85,34 @@ function Model(name,collectionRef,collectionObj){
 
 let collecNames = [];
 fs.readdirSync(collecDirectory)
- 	.forEach((filename) => {
+	.forEach((filename) => {
 		let nameArray = filename.split('.');
-		if (nameArray[nameArray.length-1]==='json'){
-			let tmp = require(path.join('../../'+collecDirectory,nameArray[0]));
-			if(typeof tmp === "array"||typeof tmp ==="object"){
-					let tempName = nameArray[0].charAt(0).toUpperCase() + nameArray[0].slice(1);
-					global[tempName] = new Model(tempName,path.join(collecDirectory,filename),tmp);
-					collecNames.push(filename);
-				}
+		if (nameArray[nameArray.length - 1] === 'json') {
+			let tmp = require(path.join('../../' + collecDirectory, nameArray[0]));
+			if (typeof tmp === "array" || typeof tmp === "object") {
+				let tempName = nameArray[0].charAt(0).toUpperCase() + nameArray[0].slice(1);
+				global[tempName] = new Model(tempName, path.join(collecDirectory, filename), tmp);
+				collecNames.push(filename);
 			}
-		else{return};
+		}
+		else { return };
 	});
 
-function Schema (schemaObject){
 
+function Schema(schema) {
+	Model.call(this,)
+	let entries = Object.entries(schema);
+	entries.forEach((valArr, index) => {
+
+		valArr[1]
+	})
+
+	//Object.getPrototypeOf(submitted value).constructor === valArr[1]
 	//should take in a schema-type object, and create a new model with type restrictions for data entry
 	//we can use object.entries to grab each of the object's k-v pairs
 	//iterate through the pairs, check type entry, may have to edit the .insertMethod? 
-	
+
 };
 
-console.log('badcube successfully imported',collecNames);
+console.log('badcube successfully imported', collecNames);
 exports.collections = collection;
